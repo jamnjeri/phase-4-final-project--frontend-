9a8afcdc088e97ac3812d09c2c96ef5452b6da91
Stellanyambura <stellagitonga.com>
2023-03-28 21:00:54 +0300

    fixed bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import About from './About';
+// import React from 'react';
+// import { Link } from 'react-router-dom';
+// import About from './About';
 
-function Navbar() {
-  return (
-    <nav>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/recipes">Recipes</Link></li>
-        <li><Link to="/about">About</Link></li>
-      </ul>
-    </nav>
-  );
-}
+// function Navbar() {
+//   return (
+//     <nav>
+//       <ul>
+//         <li><Link to="/">Home</Link></li>
+//         <li><Link to="/recipes">Recipes</Link></li>
+//         <li><Link to="/about">About</Link></li>
+//       </ul>
+//     </nav>
+//   );
+// }
 
-export default Navbar;
+// export default Navbar;
