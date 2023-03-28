77fb053e7866083f3bbe477687e2361386c68579
Stellanyambura <stellagitonga.com>
2023-03-28 21:13:52 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -5,45 +5,22 @@ import "./App.css";
 import "semantic-ui-css/semantic.min.css";
 
 
-import About from "./components/About.js";
+import About from "./components/About";
 import Skills from "./components/Skills";
 
 import { useEffect, useState } from "react";
 
 function App() {
    
-    const [currentUserEmail, setCurrentUser] = useState('');
-
-
-  useEffect(() => {
-    const storedEmail = localStorage.getItem("currentUserEmail");
-    if (storedEmail) {
-      setCurrentUser(storedEmail);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem("currentUserEmail", currentUserEmail);
-  }, [currentUserEmail]);
-
-
-  function storeEmail(value) {
-    console.log(value)
-    setCurrentUser(value);
-  }
-
-  console.log(currentUserEmail);
+    
     
   return (
     <div>
       <BrowserRouter>
         <Nav />
         <Switch>
-          <Route exact path="/"><Home/></Route>
-          <Route path="/about"><Skills/></Route>
-          <Route path="/signUp"><SignUp/></Route>
-          <Route path="/projects"><ProjectList currentUserEmail={currentUserEmail}/></Route>
-        </Switch>
+          <Route exact path="/about"><About/></Route>
+</Switch>
       </BrowserRouter>
     </div>
   );
