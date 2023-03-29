b499de567ced6c2a2185977808106b7a136290ca
ittstevin <tevingichoya@gmail.com>
2023-03-29 15:51:05 +0300

    well commented code

--- src/App.js
+++ src/App.js
@@ -5,29 +5,37 @@ import { Register } from "./routes/Register";
 import Home from "./routes/Home";
 
 function App() {
+  // Initialize state variables
   const [currentForm, setCurrentForm] = useState("login");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // Function to switch between login and register forms
   const toggleForm = (formName) => {
     setCurrentForm(formName);
   };
 
+  // Function to handle successful login
   const handleLogin = () => {
     setIsLoggedIn(true);
   };
 
+  // Function to handle logout
   const handleLogout = () => {
     setIsLoggedIn(false);
   };
 
   return (
     <div className="App">
+      {/* Render home component if user is logged in */}
       {isLoggedIn ? (
         <Home onLogout={handleLogout} />
-      ) : currentForm === "login" ? (
-        <Login onFormSwitch={toggleForm} onLogin={handleLogin} />
       ) : (
-        <Register onFormSwitch={toggleForm} onLogin={handleLogin} />
+        // Render login or register form, depending on currentForm state
+        currentForm === "login" ? (
+          <Login onFormSwitch={toggleForm} onLogin={handleLogin} />
+        ) : (
+          <Register onFormSwitch={toggleForm} onLogin={handleLogin} />
+        )
       )}
     </div>
   );
--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -1,54 +1,126 @@
 import React, { useState } from "react";
 
-export const Register = (props) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [name, setName] = useState('');
-    const [phoneNumber, setPhoneNumber] = useState('');
-    const [age, setAge] = useState('');
-    const [validationError, setValidationError] = useState('');
+export const Register = ({ onLogin, onFormSwitch }) => {
+  // Initialize state fields using a single useState hook
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+    name: '',
+    phoneNumber: '',
+    age: '',
+  });
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        
-        if (password !== confirmPassword) {
-          setValidationError("Passwords do not match");
-          return;
-        }
-        
-        console.log(email);
-        props.onLogin();
+  // Initialize validation error state to an empty string
+  const [validationError, setValidationError] = useState('');
+
+  // Update form data when input field changes
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  // Handle form submission
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Check if password and confirm password match
+    if (formData.password !== formData.confirmPassword) {
+      // Display validation error message if passwords do not match
+      setValidationError("Passwords do not match");
+      return;
     }
 
-    return (
-        <div className="auth-form-container">
-            <h2>Register</h2>
-            <form className="register-form" onSubmit={handleSubmit}>
-                <label htmlFor="name">Full name</label>
-                <input value={name} name="name" onChange={(e) => setName(e.target.value)} id="name" placeholder="Full Name" required />
-                
-                <label htmlFor="email">Email</label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@gmail.com" id="email" name="email" required />
-                
-                <label htmlFor="password">Password</label>
-                <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="********" id="password" name="password" required />
-                
-                <label htmlFor="confirm-password">Confirm Password</label>
-                <input value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} type="password" placeholder="********" id="confirm-password" name="confirm-password" required />
-                
-                <label htmlFor="phone-number">Phone Number</label>
-                <input value={phoneNumber} onChange={(e) => setPhoneNumber(e.target.value)} type="tel" placeholder="+1-555-555-1212" id="phone-number" name="phone-number" required pattern="[0-9]+" maxLength="10" title="please enter numbers only"/>
-                
-                <label htmlFor="age">Age</label>
-                <input value={age} onChange={(e) => setAge(e.target.value)} type="number" placeholder="18" id="age" name="age" required min="0" max="120"/>
+    // Call onLogin handler if form data is valid
+    onLogin();
+  };
 
-                {validationError && <div style={{color: "red"}}>{validationError}</div>}
-                
+  return (
+    <div className="auth-form-container">
+      <h2>Register</h2>
+      <form className="register-form" onSubmit={handleSubmit}>
+        {/* Use label attribute to associate input with its label */}
+        <label>
+          Full name
+          <input
+            value={formData.name}
+            onChange={handleInputChange}
+            type="text"
+            name="name"
+            placeholder="Full Name"
+            required />
+        </label>
 
-                <button type="submit">Register</button>
-            </form>
-            <button className="link-btn" onClick={() => props.onFormSwitch('login')}>Already have an account? Login here.</button>
-        </div>
-    )
-}
+        <label>
+          Email
+          <input
+            value={formData.email}
+            onChange={handleInputChange}
+            type="email"
+            placeholder="youremail@gmail.com"
+            name="email"
+            required />
+        </label>
+
+        <label>
+          Password
+          <input
+            value={formData.password}
+            onChange={handleInputChange}
+            type="password"
+            placeholder="********"
+            name="password"
+            required />
+        </label>
+
+        <label>
+          Confirm Password
+          <input
+            value={formData.confirmPassword}
+            onChange={handleInputChange}
+            type="password"
+            placeholder="********"
+            name="confirmPassword"
+            required />
+        </label>
+
+        <label>
+          Phone Number
+          <input
+            value={formData.phoneNumber}
+            onChange={handleInputChange}
+            type="tel"
+            placeholder="+1-555-555-1212"
+            name="phoneNumber"
+            required
+            pattern="[0-9]+"
+            maxLength="10"
+            title="Please enter numbers only" />
+        </label>
+
+        <label>
+          Age
+          <input
+            value={formData.age}
+            onChange={handleInputChange}
+            type="number"
+            placeholder="18"
+            name="age"
+            required
+            min="0"
+            max="120"/>
+        </label>
+
+        {/* Display validation error message if any */}
+        {validationError && <div style={{color: "red"}}>{validationError}</div>}
+
+        <button type="submit">Register</button>
+      </form>
+
+      {/* Use onClick event handler to switch to login form */}
+      <button className="link-btn" onClick={() => onFormSwitch('login')}>
+        Already have an account? Login here.
+      </button>
+    </div>
+  );
+};
