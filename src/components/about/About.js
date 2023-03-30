ff6e6a7f8cfdf49ef6373811443da84b829aeb98
Stellanyambura <stellagitonga.com>
2023-03-30 22:51:56 +0300

    eddits

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,6 +1,6 @@
+
 import React from 'react';
 import Footer from '../Footer/footer';
-
 
 function About() {
   return (
@@ -9,10 +9,111 @@ function About() {
       <p>Foodie is a web application that helps users find and discover new recipes to try out at home. Our mission is to make cooking more accessible and enjoyable for everyone.</p>
       <p>Whether you're a seasoned chef or a beginner in the kitchen, Foodie has something for you. Browse our extensive collection of recipes, save your favorites, and even create your own custom meal plans.</p>
       <p>Thank you for choosing Foodie for your cooking needs.🍗🥙🍕🍟🌭 Happy cooking!</p>
-  <Footer/>
+      <Footer className="footer" />
     </div>
   );
 }
-         
 
 export default About;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import Footer from '../Footer/footer';
+
+// function Section({ title, children }) {
+//   return (
+//     <div className="section">
+//       <h3>{title}</h3>
+//       <p>{children}</p>
+//     </div>
+//   );
+// }
+
+// function About() {
+//   return (
+//     <div className="about-container">
+//       <Section title="About Foodie 👩‍🍳🧑🏼‍🍳">
+//         Foodie is a web application that helps users find and discover new recipes to try out at home. Our mission is to make cooking more accessible and enjoyable for everyone.
+//       </Section>
+//       <Section title="Our Mission 🌟">
+//         Whether you're a seasoned chef or a beginner in the kitchen, Foodie has something for you. Browse our extensive collection of recipes, save your favorites, and even create your own custom meal plans.
+//       </Section>
+//       <Section title="Thank You! 🙏">
+//         Thank you for choosing Foodie for your cooking needs.🍗🥙🍕🍟🌭 Happy cooking!
+//       </Section>
+//       <Footer/>
+//     </div>
+//   );
+// }
+
+// export default About;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import Footer from '../Footer/footer';
+
+
+// function About() {
+//   return (
+//     <div className="about-container">
+//       <h2>About Foodie 👩‍🍳🧑🏼‍🍳</h2>
+//       <p>Foodie is a web application that helps users find and discover new recipes to try out at home. Our mission is to make cooking more accessible and enjoyable for everyone.</p>
+//       <p>Whether you're a seasoned chef or a beginner in the kitchen, Foodie has something for you. Browse our extensive collection of recipes, save your favorites, and even create your own custom meal plans.</p>
+//       <p>Thank you for choosing Foodie for your cooking needs.🍗🥙🍕🍟🌭 Happy cooking!</p>
+//   <Footer/>
+//     </div>
+//   );
+// }
+         
+
+// export default About;
